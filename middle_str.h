68f5836e76a89c691b86a1c17faa6b61ef3124fc
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-12-11 11:09:30 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -4,5 +4,6 @@
 bool itc_isDigit(unsigned char c);
 unsigned char itc_toUpper(unsigned char c);
 unsigned char itc_chargeCase(unsigned char c);
+bool itc_Compare(string s1, string s2);
 
 #endif // MIDDLE_STR_H_INCLUDED

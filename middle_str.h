6ef202385c34a5c4862c851f0f4f88412fd70c87
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-12-24 20:17:38 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -1,5 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
 #ifndef MIDDLE_STR_H_INCLUDED
 #define MIDDLE_STR_H_INCLUDED
+
 
 bool itc_isDigit(unsigned char c);
 unsigned char itc_toUpper(unsigned char c);

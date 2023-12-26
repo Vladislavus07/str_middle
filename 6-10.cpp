bb696a4a6b54b151c13d8330755283f4a74b9065
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-12-26 21:38:10 +0300

    Update 6-10.cpp

--- 6-10.cpp
+++ 6-10.cpp
@@ -3,5 +3,26 @@
 #include "middle_str.h"
 using namespace std;
 
-int main(){
+long long itc_len(string str)
+{
+	int a = 0;
+	for (int b = 0; str[b] != '\0'; b++)
+	{
+        	a++;
+	}
+	return a;
+}
+
+char itc_sameChar(string str) {
+    for(int i = 0; i < itc_len(str); i++)
+    {
+        int n = 2;
+        for (int j = i; j < itc_len(str); j++)
+            if (str[j] == str[i] && str[j] != ' ')
+            {
+                n--;
+                if (n == 0) 
+                    return str[j];
+            }
+    }
 }
